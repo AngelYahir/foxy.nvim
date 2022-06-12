22454cd8703c18cf32d577dfebd5b42f8ec0c410
AngelYahir <angel_torres2702@outlook.com>
2022-06-12 17:57:38 -0500

    Dashboard config updated

---
+++ lua/plugSettings.lua
@@ -0,0 +1,14 @@
+require('plugins/telescope')
+require('plugins/lualine')
+require('plugins/notify')
+require('plugins/identBlackline')
+require('plugins/bufferline')
+require('plugins/dashboard')
+require('plugins/treesitter')
+require('plugins/lsp')
+require('plugins/cmp-lua')
+
+vim.g.minimap_width = 10
+vim.g.minimap_auto_start = 1
+vim.g.minimap_auto_start_win_enter = 1
+
---
+++ lua/themes.lua
@@ -0,0 +1,80 @@
+--[[
+
+  ▄▄▄█████▓ ██░ ██ ▓█████  ███▄ ▄███▓▓█████   ██████
+  ▓  ██▒ ▓▒▓██░ ██▒▓█   ▀ ▓██▒▀█▀ ██▒▓█   ▀ ▒██    ▒
+  ▒ ▓██░ ▒░▒██▀▀██░▒███   ▓██    ▓██░▒███   ░ ▓██▄
+  ░ ▓██▓ ░ ░▓█ ░██ ▒▓█  ▄ ▒██    ▒██ ▒▓█  ▄   ▒   ██▒
+    ▒██▒ ░ ░▓█▒░██▓░▒████▒▒██▒   ░██▒░▒████▒▒██████▒▒
+    ▒ ░░    ▒ ░░▒░▒░░ ▒░ ░░ ▒░   ░  ░░░ ▒░ ░▒ ▒▓▒ ▒ ░
+      ░     ▒ ░▒░ ░ ░ ░  ░░  ░      ░ ░ ░  ░░ ░▒  ░ ░
+      ░       ░  ░░ ░   ░   ░      ░      ░   ░  ░  ░
+    ░  ░  ░   ░  ░       ░      ░  ░      ░
+
+--]]
+
+-- Catppuccin --
+
+-- local catppuccin = require("catppuccin")
+-- vim.cmd[[colorscheme catppuccin]]
+
+--     end     --
+
+-- TokyoNight --
+
+local tokyonight = require("tokyonight")
+vim.g.tokyonight_style = "night"
+vim.cmd[[colorscheme tokyonight]]
+
+--     end     --
+
+-- Kanagawa --
+
+-- vim.cmd[[colorscheme kanagawa]]
+
+--    end    --
+
+-- Night Fox --
+
+--vim.cmd[[colorscheme nightfox]]
+
+--   end    --
+
+-- Rose Pine --
+
+-- require('rose-pine').setup({
+-- 	---@usage 'main'|'moon'
+-- 	dark_variant = 'main',
+-- 	bold_vert_split = false,
+-- 	dim_nc_background = false,
+-- 	disable_background = false,
+-- 	disable_float_background = false,
+-- 	disable_italics = false,
+-- 	---@usage string hex value or named color from rosepinetheme.com/palette
+-- 	groups = {
+-- 		border = 'highlight_med',
+-- 		comment = 'muted',
+-- 		link = 'iris',
+-- 		punctuation = 'subtle',
+--
+-- 		error = 'love',
+-- 		hint = 'iris',
+-- 		info = 'foam',
+-- 		warn = 'gold',
+--
+-- 		headings = {
+-- 			h1 = 'iris',
+-- 			h2 = 'foam',
+-- 			h3 = 'rose',
+-- 			h4 = 'gold',
+-- 			h5 = 'pine',
+-- 			h6 = 'foam',
+-- 		}
+-- 		-- or set all headings at once
+-- 		-- headings = 'subtle'
+-- 	}
+-- })
+--
+-- -- set colorscheme after options
+--vim.cmd[[colorscheme rose-pine]]
+
+--    end    --
